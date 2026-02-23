52d5ad3f87b96c56297b5d203698876d9ce3b7cb
Guillaume HUSTA <guillaume.husta@gmail.com>
2026-02-23 14:36:09 +0100

    feat: Kotlin migration
    Closes: #55

--- app/src/main/java/fr/husta/android/dockersearch/AppConstants.kt
+++ app/src/main/java/fr/husta/android/dockersearch/AppConstants.kt
@@ -5,15 +5,15 @@ object AppConstants {
     /**
      * https://developer.chrome.com/multidevice/android/customtabs
      */
-    const val USE_CHROME_CUSTOM_TABS: Boolean = true
+    const val USE_CHROME_CUSTOM_TABS = true
 
     /**
      * Default page size for [fr.husta.android.dockersearch.docker.DockerSearchRestService.searchImages].
      * <br></br>
      * MAX = 100.
      */
-    const val IMAGE_SEARCH_PAGE_SIZE: Int = 30
+    const val IMAGE_SEARCH_PAGE_SIZE = 30
 
-    const val TAG_LIST_PAGE_SIZE: Int = 50
+    const val TAG_LIST_PAGE_SIZE = 50
 
 }
