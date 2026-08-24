618c62d2ec084541555c20884a9beb2629b338d1
Guillaume HUSTA <guillaume.husta@gmail.com>
2026-08-24 15:49:58 +0200

    Bump project version to 1.28.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "fr.husta.android.dockersearch"
         minSdk = 26 // O 8.0
         targetSdk = 36 // Android 16
-        versionCode = 157
-        versionName = "1.27.1"
+        versionCode = 158
+        versionName = "1.28.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
